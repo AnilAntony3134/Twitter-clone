4cf5342946e82368974dfe6a77a0107923597aa8
Anil Antony <anilantony3134@gmail.com>
2023-05-12 10:22:50 +0530

    feat: mutation added

--- src/server/api/routers/post.ts
+++ src/server/api/routers/post.ts
@@ -1,7 +1,8 @@
 import type { User } from "@clerk/nextjs/dist/api";
 import { clerkClient } from "@clerk/nextjs/server";
 import { TRPCError } from "@trpc/server";
-import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
+import { z } from "zod";
+import { PrivateProcedure, createTRPCRouter, publicProcedure } from "~/server/api/trpc";
 
 const filterUserForClient = (user: User) => {
     return { id: user.id, userName: user.username, profilePicture: user.profileImageUrl }
@@ -30,5 +31,22 @@ export const postRouter = createTRPCRouter({
                 userName: author.userName
             }
     }});
-    })
+    }),
+
+    create: PrivateProcedure
+        .input(
+            z.object({
+                content: z.string().min(1).max(280),
+            })
+        )
+        .mutation(async ({ctx, input}) => {
+            const authorId = ctx.userId;
+            const post = await ctx.prisma.post.create({
+                data: {
+                    authorId,
+                    content: input.content
+                }
+            })
+            return post;
+        })
 })
