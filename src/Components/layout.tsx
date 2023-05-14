e186a65b32a2a1cfc274e4810cf0dec39bfe1fa2
Anil Antony <anilantony3134@gmail.com>
2023-05-14 14:18:08 +0530

    feat: profile and layout added

---
+++ src/Components/layout.tsx
@@ -0,0 +1,10 @@
+import { type PropsWithChildren } from "react";
+
+export const PageLayout = (props: PropsWithChildren) => (
+    <main className="flex h-screen justify-center">
+        <div className="h-full w-full border-x border-slate-400 md:max-w-2xl overflow-y-scroll">
+            {props.children}
+        </div>
+    </main>
+
+)
