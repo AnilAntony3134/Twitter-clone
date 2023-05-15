a0bced041d59d6bcc447b3af738b5ad39820c972
Anil Antony <anilantony3134@gmail.com>
2023-05-15 07:14:09 +0530

    fix: import slug route

--- src/pages/[slug].tsx
+++ src/pages/[slug].tsx
@@ -4,9 +4,9 @@ import Head from "next/head";
 import { api } from "~/utils/api";
 import Image from "next/image";
 import { generateSSGHelper } from "~/server/api/helpers/ssgHelper";
-import { LoadingPage } from "~/components/loading";
+import { LoadingPage } from "../components/loading";
 import { PageLayout } from "../components/layout";
-import { PostView } from "~/components/postView";
+import { PostView } from "../components/postView";
 
 const ProfileFeed = (props: {userId: string}) => {
     const {data, isLoading} = api.posts.getPostsByUserId.useQuery({
